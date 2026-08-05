ddcd85c3f3226e8e3121abce293cd1fd389e835d
https://github.com/RubyBola/examprep-backend.git <oyadiranbolarinwa1994@gmail.com>
2026-08-05 02:48:27 +0100

    modified utils

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -15,21 +15,27 @@ const transporter = nodemailer.createTransport({
 await transporter.verify();
 console.log("SMTP connection successful");
 export const sendVerificationEmail = async (email, token) => {
-  console.log("8. Entered sendVerificationEmail");
-  const info = await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Verify your email",
-    html: `
-      <h2>Welcome to ExamPrep</h2>
-      <p>Thank you for registering.</p>
-      <p>Your verification token is:</p>
-      <h3>${token}</h3>
-      <p>Use this token to verify your account.</p>
-    `,
-  });
-   console.log("9. Mail sent:", info.messageId);
-   return info;
-};
+  try {
+    console.log("8. Entered sendVerificationEmail");
 
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Verify your email",
+      html: `
+        <h2>Welcome to ExamPrep</h2>
+        <p>Thank you for registering.</p>
+        <p>Your verification token is:</p>
+        <h3>${token}</h3>
+        <p>Use this token to verify your account.</p>
+      `,
+    });
+
+    console.log("9. Mail sent:", info.messageId);
+    return info;
+  } catch (error) {
+    console.error("❌ sendMail failed:", error);
+    throw error;
+  }
+};
 export default sendVerificationEmail;
