618b058dca57961b9f0929982df442b6ba26e845
https://github.com/RubyBola/examprep-backend.git <oyadiranbolarinwa1994@gmail.com>
2026-08-05 10:13:32 +0100

    modified

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,9 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
