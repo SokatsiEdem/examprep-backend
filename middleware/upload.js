84fa49ae70b59030747c052f2eeddb82d34c8711
https://github.com/RubyBola/examprep-backend.git <oyadiranbolarinwa1994@gmail.com>
2026-08-06 10:52:20 +0100

    modified

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,30 +1,22 @@
+import fs from "fs";
 import multer from "multer";
+
+const uploadDir = "uploads";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename(req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-const upload = multer({
-  storage,
-
-  fileFilter(req, file, cb) {
-    if (
-      file.mimetype ===
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
-      file.mimetype ===
-        "application/vnd.ms-excel"
-    ) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only Excel files are allowed."));
-    }
-  },
-});
+const upload = multer({ storage });
 
 export default upload;
