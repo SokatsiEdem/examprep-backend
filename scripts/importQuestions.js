2760169409c29766e6820579887d43a4b5a81f88
https://github.com/RubyBola/examprep-backend.git <oyadiranbolarinwa1994@gmail.com>
2026-08-06 19:14:18 +0100

    modified

--- scripts/importQuestions.js
+++ scripts/importQuestions.js
@@ -1,65 +1,51 @@
 import XLSX from "xlsx";
 import prisma from "../config/prisma.js";
 
-const workbook = XLSX.readFile("./data/question.xlsx");
+export const importQuestions = async (filePath) => {
+  const workbook = XLSX.readFile(filePath);
 
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  const rows = XLSX.utils.sheet_to_json(sheet);
 
-const rows = XLSX.utils.sheet_to_json(sheet);
-
-async function importQuestions() {
   let imported = 0;
   let skipped = 0;
 
   for (const row of rows) {
-    try {
-      const exists = await prisma.question.findFirst({
-        where: {
-          questionText: row.questionText,
-          year: Number(row.year),
-        },
-      });
-
-      if (exists) {
-        skipped++;
-        continue;
-      }
-
-      await prisma.question.create({
-        data: {
-          examType: row.examType,
-          subject: row.subject,
-          topic: row.topic,
-          year: Number(row.year),
-          questionText: row.questionText,
-
-          questionImage: row.questionImage || null,
-
-          options: {
-            A: row.optionA,
-            B: row.optionB,
-            C: row.optionC,
-            D: row.optionD,
-          },
-
-          correctOption: row.correctOption,
-
-          explanation: row.explanation,
-
-          explanationImage: row.explanationImage || null,
-        },
-      });
-
-      imported++;
-    } catch (err) {
-      console.error(err);
+    const exists = await prisma.question.findFirst({
+  where: {
+    questionText: row.questionText,
+  },
+});
+    if (exists) {
+      skipped++;
+      continue;
     }
+
+    await prisma.question.create({
+  data: {
+    examType: row.examType,
+    subject: row.subject,
+    topic: row.topic,
+    questionText: row.questionText,
+    questionImage: row.questionImage || null,
+    options: {
+      A: row.optionA,
+      B: row.optionB,
+      C: row.optionC,
+      D: row.optionD,
+    },
+    correctOption: row.correctOption,
+    explanation: row.explanation,
+    explanationImage: row.explanationImage || null,
+  },
+});
+
+    imported++;
   }
 
-  console.log(`Imported: ${imported}`);
-  console.log(`Skipped: ${skipped}`);
-
-  await prisma.$disconnect();
-}
-
-importQuestions();
+  return {
+    success: true,
+    imported,
+    skipped,
+  };
+};
